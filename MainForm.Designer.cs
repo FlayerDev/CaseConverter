87084fb3d624f91ab515b55d48d8648b3af6cc2b
FlayerDev <flayer.dev.gr@gmail.com>
2021-02-17 19:48:00 +0200

    Almost done with the main functions

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -37,22 +37,17 @@
             this.AlternatingCase = new System.Windows.Forms.Button();
             this.InverseCase = new System.Windows.Forms.Button();
             this.UpperCaseButton = new System.Windows.Forms.Button();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainTextBox
             // 
             this.MainTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.MainTextBox.Location = new System.Drawing.Point(0, 0);
+            this.MainTextBox.Location = new System.Drawing.Point(0, 64);
             this.MainTextBox.Multiline = true;
             this.MainTextBox.Name = "MainTextBox";
             this.MainTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
-            this.MainTextBox.Size = new System.Drawing.Size(752, 311);
+            this.MainTextBox.Size = new System.Drawing.Size(752, 378);
             this.MainTextBox.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -75,9 +70,10 @@
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(752, 32);
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(752, 64);
             this.tableLayoutPanel1.TabIndex = 8;
             // 
             // CapitalizedCase
@@ -111,6 +107,7 @@
             this.SentenceCase.TabIndex = 4;
             this.SentenceCase.Text = "Sentence Case";
             this.SentenceCase.UseVisualStyleBackColor = true;
+            this.SentenceCase.Click += new System.EventHandler(this.SentenceCase_Click);
             // 
             // TitleCase
             // 
@@ -156,41 +153,19 @@
             this.UpperCaseButton.UseVisualStyleBackColor = true;
             this.UpperCaseButton.Click += new System.EventHandler(this.UpperCaseButton_Click);
             // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.AutoScroll = true;
-            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.MainTextBox);
-            this.splitContainer1.Size = new System.Drawing.Size(752, 442);
-            this.splitContainer1.SplitterDistance = 127;
-            this.splitContainer1.TabIndex = 1;
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(752, 442);
-            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.MainTextBox);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.MinimumSize = new System.Drawing.Size(760, 400);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            this.splitContainer1.Panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -205,7 +180,6 @@
         private System.Windows.Forms.Button SentenceCase;
         private System.Windows.Forms.Button LowerCaseButton;
         private System.Windows.Forms.Button CapitalizedCase;
-        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -39,12 +39,43 @@ namespace CaseConverter
 
         private void CapitalizedCase_Click(object sender, EventArgs e)
         {
-
+            bool capNext = true;
+            string textBuffer = string.Empty;
+            foreach (char item in MainTextBox.Text)
+            {
+                if (item == (char)32)
+                {
+                    capNext = true;
+                    textBuffer += (char)32;
+                }
+                else
+                {
+                    if (capNext) textBuffer += char.ToUpper(item);
+                    else textBuffer += char.ToLower(item);
+                    capNext = false;
+                }
+            }
+            MainTextBox.Text = textBuffer;
         }
 
         private void InverseCase_Click(object sender, EventArgs e)
         {
-
+            bool capNext = true;
+            string textBuffer = string.Empty;
+            foreach (char item in MainTextBox.Text)
+            {
+                if (item == (char)32)
+                {
+                    capNext = true;
+                    textBuffer += (char)32;                }
+                else
+                {
+                    if (capNext) textBuffer += char.ToLower(item);
+                    else textBuffer += char.ToUpper(item);
+                    capNext = false;
+                }
+            }
+            MainTextBox.Text = textBuffer;
         }
 
         private void TitleCase_Click(object sender, EventArgs e)
@@ -54,7 +85,37 @@ namespace CaseConverter
 
         private void AlternatingCase_Click(object sender, EventArgs e)
         {
+            string textBuffer = string.Empty;
+            for (int i = 0; i < MainTextBox.Text.Length; i++)
+            {
+                if (i % 2 != 0) textBuffer += char.ToUpper(MainTextBox.Text[i]);
+                else textBuffer += char.ToLower(MainTextBox.Text[i]);
+            }
+        }
 
+        private void SentenceCase_Click(object sender, EventArgs e)
+        {
+            bool capNext = true;
+            string textBuffer = string.Empty;
+            foreach (char item in MainTextBox.Text)
+            {
+                if (item == '.')
+                {
+                    capNext = true;
+                    textBuffer += '.';
+                }
+                else if (item == (char)32)
+                {
+                    textBuffer += (char)32;
+                }
+                else
+                {
+                    if (capNext) textBuffer += char.ToUpper(item);
+                    else textBuffer += char.ToLower(item);
+                    capNext = false;
+                }
+            }
+            MainTextBox.Text = textBuffer;
         }
     }
 }
